6a4fed39fe378a25241b93e1119acca68f1ba607
mapcrafter2048 <121042282+mapcrafter2048@users.noreply.github.com>
2024-02-13 01:55:40 +0530

    Added the language button, Env file

---
+++ edit_x/app/components/languageDropDown.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { useState } from "react";
+import {
+  Dropdown,
+  DropdownTrigger,
+  DropdownMenu,
+  DropdownItem,
+  Button,
+} from "@nextui-org/react";
+import { languageOptions } from "../Constants/languages";
+
+export default function DropdownLanguage() {
+  const [selectedLanguage, setSelectedLanguage] = useState(
+    languageOptions[0].value
+  );
+
+  const handleLanguageChange = (value: string) => {
+    const selectedOption = languageOptions.find(
+      (option) => option.value === value
+    );
+    setSelectedLanguage(selectedOption!.value);
+  };
+  return (
+    <Dropdown>
+      <DropdownTrigger>
+        <Button variant="bordered">Select Language</Button>
+      </DropdownTrigger>
+      <DropdownMenu aria-label="Static Actions">
+        {languageOptions.map((option) => (
+          <DropdownItem
+            key={option.value}
+            onClick={() => handleLanguageChange(option.value)}
+          >
+            {option.name}
+          </DropdownItem>
+        ))}
+      </DropdownMenu>
+    </Dropdown>
+  );
+}
--- edit_x/app/page.tsx
+++ edit_x/app/page.tsx
@@ -1,32 +1,57 @@
-"use client"
+"use client";
 import Image from "next/image";
-import Editor from '@monaco-editor/react';
+import Editor from "@monaco-editor/react";
 import { useState } from "react";
-import {  Button, Dropdown, DropdownTrigger,  DropdownMenu,  DropdownSection,  DropdownItem} from "@nextui-org/react";
+import {
+  Button,
+  Dropdown,
+  DropdownTrigger,
+  DropdownMenu,
+  DropdownSection,
+  DropdownItem,
+} from "@nextui-org/react";
 // import {Button, ButtonGroup} from "@nextui-org/react";
+import DropdownLanguage from "./components/languageDropDown";
+import { languageOptions } from "./Constants/languages";
+import axios from "axios";
+import { Source_Code_Pro } from "next/font/google";
 
 export default function Home() {
   const [fontSize, setFontSize] = useState(30);
   return (
     <main>
-      <nav className="flex flex-row gap-4 items-center h-10"><span>Editor v0.1</span>
-      <Dropdown>
-      <DropdownTrigger>
-        <Button 
-          variant="bordered"
-          className="w-20"
-        >
-          Font Size: {fontSize}
-        </Button>
-      </DropdownTrigger>
-      <DropdownMenu aria-label="Static Actions">
-        <DropdownItem key="20" onPress={()=>setFontSize(20)}>20</DropdownItem>
-        <DropdownItem key="30" onPress={()=>setFontSize(30)}>30</DropdownItem>
-        <DropdownItem key="40" onPress={()=>setFontSize(40)}>40</DropdownItem>
-      </DropdownMenu>
-    </Dropdown>
-    </nav>
-      <div><Editor options={{fontSize:fontSize}} width="100%" theme="vs-dark" height="100vh" defaultLanguage="javascript" defaultValue="// some comment" /></div>
+      <nav className="flex flex-row gap-4 items-center h-10">
+        <span>Editor v0.1</span>
+        <Dropdown>
+          <DropdownTrigger>
+            <Button variant="bordered" className="w-20">
+              Font Size: {fontSize}
+            </Button>
+          </DropdownTrigger>
+          <DropdownMenu aria-label="Static Actions">
+            <DropdownItem key="20" onPress={() => setFontSize(20)}>
+              20
+            </DropdownItem>
+            <DropdownItem key="30" onPress={() => setFontSize(30)}>
+              30
+            </DropdownItem>
+            <DropdownItem key="40" onPress={() => setFontSize(40)}>
+              40
+            </DropdownItem>
+          </DropdownMenu>
+        </Dropdown>
+      </nav>
+      <div>
+        <Editor
+          options={{ fontSize: fontSize }}
+          width="100%"
+          theme="vs-dark"
+          height="100vh"
+          defaultLanguage="javascript"
+          defaultValue="// some comment"
+        />
+      </div>
+      <DropdownLanguage />
     </main>
   );
 }
