9f3b68dee55af6f2b5f22e8230480e64f4f715fe
Aditya Kshitiz <adityakshitiz2017@gmail.com>
2024-02-16 22:19:50 +0530

    fixed problems caused by abhinav pushing

--- edit_x/app/page.tsx
+++ edit_x/app/page.tsx
@@ -1,10 +1,12 @@
 "use client";
 import Editor from "@monaco-editor/react";
+import { io } from "socket.io-client";
 import { PlayArrow } from "@mui/icons-material";
 import { useState, useRef, useEffect } from "react";
 import axios from "axios";
 import { languageOptions } from "./constants/languages";
 import {
+  Textarea,
   Button,
   Dropdown,
   DropdownTrigger,
@@ -12,33 +14,77 @@ import {
   DropdownSection,
   DropdownItem,
   Spinner,
-  Listbox,
-  ListboxSection,
-  ListboxItem,
+  Card,
+  CardBody,
 } from "@nextui-org/react";
 import SplitPane, { Pane } from "split-pane-react";
 import "split-pane-react/esm/themes/default.css";
 import "react-grid-layout/css/styles.css";
 import "react-resizable/css/styles.css";
-import DescriptionIcon from "@mui/icons-material/Description";
+import InputModal from "./components/InputModal";
+
+// import LanguageSelect from "./components/languageDropDown";
+// import FontSizeSelect from "./components/Fontsize";
+
+// const lang = {
+//   Python: {
+//     language: "python",
+//     value: `print("Hello there!")`,
+//   },
+//   HTML: {
+//     language: "html",
+//     value: "<div> </div>",
+//   },
+//   Javascript: {
+//     language: "javascript",
+//     value: `console.log("Hi")`,
+//   },
+// };
+
+const socket = io("http://localhost:8000", {
+  autoConnect: false,
+});
 
 export default function Home() {
   const [fontSize, setFontSize] = useState(30);
   const editorRef = useRef(null);
+  // const [editorText, setEditorText] = useState("");
   const [currLanguage, setCurrLanguage] = useState(0);
   const [sizes, setsizes] = useState([350, "30%", "auto"]);
   const [nestedSizes, setNestedSizes] = useState([20, 80]);
-  const [pane2Sizes, setPane2Sizes] = useState([50, 50]);
   const [stdinValue, setStdinValue] = useState("");
   const [executionResult, setExecutionResult] = useState("");
+  const [pane2Sizes, setPane2Sizes] = useState([50, 50]);
+  const [isLoading, setIsLoading] = useState(false);
+  // console.log(languageOptions);
+  // console.log((editorRef.current==null)?("null"):(editorRef.current.getValue()));
+  if (editorRef.current != null) {
+    editorRef.current.onKeyDown(() => console.log("keyPress"));
+  }
+  useEffect(() => {
+    socket.connect();
+    socket.on("connect", () => {
+      console.log("socket.io connected successfully");
+    });
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
 
-  const layoutCSS = {
-    height: "100%",
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
+  useEffect(() => {
+    socket.on("text-update", (res) => {
+      console.log(res);
+      if (res !== editorRef.current.getValue()) {
+        const cursorInfo = editorRef.current.getPosition();
+        console.log(cursorInfo);
+        editorRef.current.setValue(res);
+        editorRef.current.setPosition(cursorInfo);
+      }
+    });
+  }, [socket]);
+  const handleEditorChange = (value, event) => {
+    socket.emit("code-value", value);
   };
-
   const submitCode = () => {
     axios
       .post("http://localhost:2358/submissions", {
@@ -62,16 +108,18 @@ export default function Home() {
         return res.data.token;
       })
       .then((res) => {
+        console.log(editorRef.current.getPosition());
         setTimeout(() => {
-          axios
-            .get(`http://localhost:2358/submissions/${res}`)
-            .then((res) => setExecutionResult(res.data.stdout));
+          axios.get(`http://localhost:2358/submissions/${res}`).then((res) => {
+            setIsLoading(false);
+            setExecutionResult(res.data.stdout);
+          });
         }, 5000);
       });
   };
   return (
     <main>
-      <div className="flex flex-row gap-4 items-center p-4">
+      <div className="flex flex-row gap-4 items-center p-2">
         <span>Editor v0.1</span>
         <Dropdown>
           <DropdownTrigger>
@@ -80,9 +128,6 @@ export default function Home() {
             </Button>
           </DropdownTrigger>
           <DropdownMenu aria-label="Static Actions">
-            <DropdownItem key="15" onPress={() => setFontSize(15)}>
-              15
-            </DropdownItem>
             <DropdownItem key="20" onPress={() => setFontSize(20)}>
               20
             </DropdownItem>
@@ -93,12 +138,10 @@ export default function Home() {
               40
             </DropdownItem>
             <DropdownItem key="80" onPress={() => setFontSize(80)}>
-              Too large
+              It's fucking big
             </DropdownItem>
           </DropdownMenu>
         </Dropdown>
-
-        {/* <FontSizeSelect /> */}
 
         <Dropdown>
           <DropdownTrigger>
@@ -110,32 +153,31 @@ export default function Home() {
             aria-label="Static Actions"
             classNames={{ base: "h-96 overflow-auto scrollbar-hide" }}
           >
-            {languageOptions.map((Lang, index) => {
-              return (
-                <DropdownItem
-                  key={Lang.id}
-                  onPress={() => setCurrLanguage(index)}
-                >
-                  {Lang.name}
-                </DropdownItem>
-              );
-            })}
+            {languageOptions.map((Lang, index) => (
+              <DropdownItem
+                key={Lang.id}
+                onPress={() => setCurrLanguage(index)}
+              >
+                {Lang.name}
+              </DropdownItem>
+            ))}
           </DropdownMenu>
         </Dropdown>
-
-        {/* <LanguageSelect /> */}
-
+        <InputModal />
         <Button
           variant="bordered"
           className="ml-auto w-fit"
-          onPress={submitCode}
+          onPress={() => {
+            setIsLoading(true);
+            submitCode();
+          }}
         >
           {/* <Spinner/> */}
           <PlayArrow />
         </Button>
       </div>
 
-      <div style={{ height: "88vh" }}>
+      <div style={{ height: "92.5vh" }}>
         <SplitPane
           split="horizontal"
           sizes={sizes}
@@ -149,24 +191,10 @@ export default function Home() {
             sashRender={() => <div className="sash" />}
           >
             <Pane
-              style={{ ...layoutCSS }}
+              className="p-4 flex justify-center items-center h-full"
               minSize={200}
-              className="bg-gray-900"
             >
-              <div className="p-4 overflow-y-auto h-full flex flex-col space-y-2">
-                <div className="flex item-center space-x-2 text-white">
-                  <DescriptionIcon />
-                  <p>Log Entry 1</p>
-                </div>
-                <div className="flex item-center space-x-2 text-white">
-                  <DescriptionIcon />
-                  <p>Log Entry 1</p>
-                </div>
-                <div className="flex item-center space-x-2 text-white">
-                  <DescriptionIcon />
-                  <p>Log Entry 1</p>
-                </div>
-              </div>
+              pane1
             </Pane>
             <Pane minSize={200}>
               <Editor
@@ -176,8 +204,9 @@ export default function Home() {
                 height="100vh"
                 path={languageOptions[currLanguage].name}
                 defaultLanguage={languageOptions[currLanguage].value}
-                defaultValue={""}
+                defaultValue={languageOptions[currLanguage].boilerplateCode}
                 onMount={(editor, monaco) => (editorRef.current = editor)}
+                onChange={handleEditorChange}
               />
             </Pane>
           </SplitPane>
@@ -187,20 +216,43 @@ export default function Home() {
             onChange={setPane2Sizes}
             sashRender={() => <div className="sash" />}
           >
-            <Pane style={{ ...layoutCSS, background: "#A9A9A9" }} minSize={100}>
+            <Pane
+              className="p-4 pr-2 flex justify-center items-center h-full"
+              minSize={100}
+            >
               {" "}
               {/*  pane2a */}
-              <textarea
-                id="stdinInput"
-                style={{ width: "100%", height: "100%", resize: "none" }}
+              <Textarea
+                classNames={{ base: "h-full", innerWrapper: "h-lvh" }}
+                placeholder="Input"
+                size="lg"
+                fullWidth={true}
                 value={stdinValue}
-                onChange={(e) => setStdinValue(e.target.value)}
+                onValueChange={setStdinValue}
               />
             </Pane>
-            <Pane style={{ ...layoutCSS, background: "#a1a5a9" }} minSize={100}>
+            <Pane
+              className="p-4 pl-2 flex justify-center items-center h-full"
+              minSize={100}
+            >
               {" "}
               {/* pane2b */}
-              <pre>{executionResult}</pre>
+              {(isLoading)?(
+              <Card className="h-full w-full">
+                <CardBody className="flex justify-center items-center">
+                <Spinner size="lg" />
+                </CardBody>
+              </Card>
+              ):(
+              <Textarea
+                classNames={{ base: "h-full", innerWrapper: "h-lvh" }}
+                placeholder="Output"
+                size="lg"
+                fullWidth={true}
+                value={executionResult}
+                isReadOnly
+              />
+              )}
             </Pane>
           </SplitPane>
         </SplitPane>
